b4867526631a1413f67d5b83440194b751e74cdd
salikzquidz <salikinbaik@gmail.com>
2021-08-26 20:50:36 +0800

    skaffold for auth, ingress yaml,  and edit localhost file

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,5 +3,9 @@ const app = express();
 
 app.use(express.json());
 
+app.get('/api/users/currentuser', (req,res) => {
+    res.send('Current user')
+})
+
 // the port number doesnt really make any differences when using kubernetes
 app.listen(3000, ()=>{console.log('listening on port 3000')})
