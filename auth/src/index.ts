3fb06a30ab904d46f128c3656c00c3fc379d76e4
salikzquidz <salikinbaik@gmail.com>
2021-08-31 05:38:18 +0800

    add NotFoundError

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,6 +5,9 @@ import {currentUserRouter} from './routes/current-user'
 import {signUpRouter} from './routes/signup';
 import {signInRouter} from './routes/signin';
 import {signOutRouter} from './routes/signout'
+
+// not found error
+import { NotFoundError } from "./errors/not-found-error";
 // middleware
 import {errorHandler} from './middlewares/error-handler'
 
@@ -14,6 +17,12 @@ app.use(currentUserRouter);
 app.use(signUpRouter);
 app.use(signInRouter);
 app.use(signOutRouter);
+
+app.all('*', ()=> {
+    throw new NotFoundError();
+})
+// after throw, errorHandler will be used
+
 app.use(errorHandler);
 
 // the port number doesnt really make any differences when using kubernetes
