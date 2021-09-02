839ce3dfc6aea34155dc0a07263f73d8a9954cc9
salikzquidz <salikinbaik@gmail.com>
2021-09-02 19:16:04 +0800

    add static property to the model to avoid exporting 2 modules

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,38 +1,40 @@
-import {Schema, model, connect} from "mongoose";
+import {Schema, model, connect, Model} from "mongoose";
 
-interface User {
+// An interface that describes the properties required to create a new User
+
+interface UserAttrs {
     name : string,
     email : string,
     password : string
 }
 
-const schema = new Schema<User>({
+// An interface that describes the properties that a User Model has.
+// To tell TS that there is build function available on the User Model.
+
+interface UserModel extends Model<any>{
+    build(attrs : UserAttrs) : any
+}
+
+const schema = new Schema<UserAttrs>({
     // String capital 'S'
     name : { type : String, required : true},
     email : { type : String, required : true},
     password : { type : String, required : true}
 })
 
-const UserModel = model<User>('User', schema)
+const User = model<any, UserModel>('User', schema)
 
-// using 'new' keyword straight away is not recommended when creating new user
-// instead we will use buildUser function
-
-// new UserModel({
-//     name : 'Sal',
-//     email : 's@sa.com',
-//     passord : '2345'
-// })
-
-const buildUser = (userAttributes : User) => {
-    return new UserModel(userAttributes)
+// custom function into the model
+// add static properties to remove buildUser(), and to avoid export 2 things (User and buildUser, but now only User)
+// Define the static methods of our model, functions which can be called on the model itself
+schema.statics.build = (userAttributes : UserAttrs) => {
+    return new User(userAttributes)
 }
 
-// buildUser function checks the properties, try to edit the property name and you will get error
-buildUser({
-    name : 's',
-    email : 's@.com',
+User.build({
+    name : 'sad',
+    email : 's@s.com',
     password : 'sad'
 })
 
-export {UserModel, buildUser}
+export {User}
