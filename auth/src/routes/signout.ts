dc792f0f190e24afc4bd71675c16d87b0f3140fb
salikzquidz <salikinbaik@gmail.com>
2021-09-16 15:50:30 +0800

    sign out

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -3,7 +3,9 @@ import express from 'express';
 const router = express.Router();
 
 router.post('/api/users/signOut', (req,res) => {
-    res.send('router sign OUT')
+    req.session = null;
+    res.send({})
+    // res.send('router sign OUT')
 })
 
 export {router as signOutRouter};
