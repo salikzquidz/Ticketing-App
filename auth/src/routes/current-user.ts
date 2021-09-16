b9627026ecb1dce24bfdf2a9fe160155cdae2182
salikzquidz <salikinbaik@gmail.com>
2021-09-16 18:28:44 +0800

    middleware require-auth dan tambah not authorized error

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,24 +1,10 @@
 import express from 'express';
-import jwt from 'jsonwebtoken'
 import { currentUser } from '../middlewares/current-user';
-
 const router = express.Router();
 
 router.get('/api/users/currentuser', currentUser, (req,res) => {
     // req.currentUser sbb dah augment dlm middleware
     res.send({currentUser : req.currentUser || null});
-    // if(!req.session?.jwt)
-    // if(!req.session || !req.session.jwt){
-    //     return res.send({currentUser:null})
-    // }
-    // try{
-    //     const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
-    //     res.send({currentUser : payload});
-    // }catch{
-    //     res.send({currentUser : null});
-    // }
-
-    // res.send()
 })
 
 export {router as currentUserRouter};
