bd8767feddd3755b5e4ed08ac1e8c92108785b67
salikzquidz <salikinbaik@gmail.com>
2021-10-04 00:53:42 +0800

    refactor -preconfig axios

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,28 +1,15 @@
-import axios from "axios";
+import buildClient from "../api/build-client";
 
 const LandingPage = ({ currentUser }) => {
   console.log(currentUser);
-  //   axios.get("/api/users/currentuser");
-  return <h1>Landing page </h1>;
+
+  return <h1>Landing Page</h1>;
 };
 
-LandingPage.getInitialProps = async ({ req }) => {
-  if (typeof window === "undefined") {
-    // we are on the server
-    // requests should be made to http://ingress-
-    const response = await axios.get(
-      "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
-      {
-        headers: req.headers,
-      }
-    );
-    return response.data;
-  } else {
-    // we are on the browser
-    // request can be made with a base url of ''
-    const response = await axios.get("/api/users/currentuser");
-    return response.data;
-  }
-  return {};
+LandingPage.getInitialProps = async (context) => {
+  const client = buildClient(context);
+  const { data } = await client.get("/api/users/currentuser");
+  return data;
 };
+
 export default LandingPage;
