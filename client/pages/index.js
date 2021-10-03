223c059bd7fa8db03f9c56ccc4f43f7ef335c741
salikzquidz <salikinbaik@gmail.com>
2021-10-03 23:59:32 +0800

    getInitialProps

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,14 +1,30 @@
-import axios from "axios"
+import axios from "axios";
 
-const LandingPage = ({currentUser}) => {
-    console.log(currentUser)
-    axios.get('/api/users/currentuser')
-    return <h1>Landing page </h1>
-}
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
+  //   axios.get("/api/users/currentuser");
+  return <h1>Landing page </h1>;
+};
 
-LandingPage.getInitialProps = async() => {
-    // const response = await axios.get('/api/users/currentuser')
-    // return response.data;
-    return {color : 'red'}
-}
+LandingPage.getInitialProps = async () => {
+  if (typeof window === "undefined") {
+    // we are on the server
+    // requests should be made to http://ingress-
+    const response = await axios.get(
+      "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
+      {
+        headers: {
+          Host: "ticketing.dev",
+        },
+      }
+    );
+    return response.data;
+  } else {
+    // we are on the browser
+    // request can be made with a base url of ''
+    const response = await axios.get("/api/users/currentuser");
+    return response.data;
+  }
+  return {};
+};
 export default LandingPage;
