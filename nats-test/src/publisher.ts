49ba253f5ff31aa784c720c3a6c22f86ea271f68
salikzquidz <salikinbaik@gmail.com>
2021-10-14 06:29:01 +0800

    update publisher.ts

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -5,4 +5,14 @@ const stan = nats.connect("ticketing", "abc", {
 
 stan.on("connect", () => {
   console.log("Publisher connected to NATS");
+
+  const data = JSON.stringify({
+    id: 123,
+    title: "concert",
+    price: 20,
+  });
+
+  stan.publish("ticket:created", data, () => {
+    console.log("Event published");
+  });
 });
