f42a283a4a805d0f3a07369a68e8aeb07bdb4afe
salikzquidz <salikinbaik@gmail.com>
2021-10-15 06:38:15 +0800

    add abstract class for listener

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,4 +1,4 @@
-import nats, { Message } from "node-nats-streaming";
+import nats, { Message, Stan } from "node-nats-streaming";
 import { randomBytes } from "crypto";
 console.clear();
 // 123 is clientid
@@ -50,3 +50,46 @@ stan.on("connect", () => {
 // watching for interrupt signal or terminate signal
 process.on("SIGINT", () => stan.close());
 process.on("SIGTERM", () => stan.close());
+
+abstract class Listener {
+  abstract subject: string;
+  abstract queueGroupName: string;
+  private client: Stan;
+  protected ackWait: number = 5000; // 5second
+  abstract onMessage(data: any, msg: Message): void;
+
+  constructor(client: Stan) {
+    this.client = client;
+  }
+
+  subscriptionOptions() {
+    return this.client
+      .subscriptionOptions()
+      .setAckWait(this.ackWait)
+      .setManualAckMode(true)
+      .setDeliverAllAvailable()
+      .setDurableName(this.queueGroupName); // make it same with queue group name
+  }
+
+  listen() {
+    const subscription = this.client.subscribe(
+      this.subject,
+      this.queueGroupName,
+      this.subscriptionOptions()
+    );
+    subscription.on("message", (msg: Message) => {
+      console.log(
+        `Message received: ${this.subject} ,  ${this.queueGroupName}`
+      );
+      const parsedData = this.parseMessage(msg);
+      // on message
+      this.onMessage(parsedData, msg);
+    });
+  }
+  parseMessage(msg: Message) {
+    const data = msg.getData();
+    return typeof data === "string"
+      ? JSON.parse(data)
+      : JSON.parse(data.toString("utf-8")); // for buffer
+  }
+}
